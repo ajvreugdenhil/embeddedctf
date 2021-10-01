51ba62b579317a810b30f202374e909e85c7b681
Arjan <33868752+ajvreugdenhil@users.noreply.github.com>
2021-10-01 16:22:46 +0200

    uart

--- embeddedctf/src/main.cpp
+++ embeddedctf/src/main.cpp
@@ -4,15 +4,20 @@
 
 #ifdef FLAGLESS
 #define BLINKFLAG "ECTF{FLAGLESS}"
+#define UARTFLAG "ECTF{FLAGLESS}"
 #else
-#define BLINKFLAG "ECTF{}"
+#define BLINKFLAG "ECTF{TODO}"
+#define UARTFLAG "ECTF{TODO}"
 #endif
+
+#define UARTKEY "TODO"
 
 #define BLINKLED LED_BUILTIN
 
 unsigned int previous_tick = 0;
 
 void setup() {
+  Serial.begin(57600);
   pinMode(BLINKLED, OUTPUT);
 }
 
@@ -24,6 +29,40 @@ void tick_blink()
   pinMode(BLINKLED, ((blink_flag[((int)step/8)] >> (step%8))) & 1);
 }
 
+char uart_buffer[127] = {0x41};
+int uart_buffer_len = 0;
+void tick_uart()
+{
+  if (Serial.available())
+  {
+    char b = Serial.read();
+    if (b != '\n')
+    {
+      uart_buffer[uart_buffer_len] = b;
+      uart_buffer_len += 1;
+      Serial.print(b);
+    }
+    else
+    {
+      uart_buffer[uart_buffer_len - 1] = '\0';
+      if (strcmp(UARTKEY, uart_buffer) == 0)
+      {
+        Serial.print("\nCongrats! here's the flag: ");
+        Serial.println(UARTFLAG);
+      }
+      else
+      {
+        Serial.print("\nIncorrect key: '");
+        Serial.print(uart_buffer);
+        Serial.print("' \n-> ");
+      }
+      
+      uart_buffer_len = 0;
+    }
+  }
+}
+
 void loop() {
   tick_blink();
+  tick_uart();
 }
