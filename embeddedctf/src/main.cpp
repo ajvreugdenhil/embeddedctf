08eb10fcbbbb35b4d2f34497d40a4e9cf7794ae7
Arjan <33868752+ajvreugdenhil@users.noreply.github.com>
2021-10-01 15:44:06 +0200

    blinker

--- embeddedctf/src/main.cpp
+++ embeddedctf/src/main.cpp
@@ -1,9 +1,29 @@
 #include <Arduino.h>
 
+#define FLAGLESS 1
+
+#ifdef FLAGLESS
+#define BLINKFLAG "ECTF{FLAGLESS}"
+#else
+#define BLINKFLAG "ECTF{}"
+#endif
+
+#define BLINKLED LED_BUILTIN
+
+unsigned int previous_tick = 0;
+
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(BLINKLED, OUTPUT);
+}
+
+void tick_blink()
+{
+  char blink_flag[] = BLINKFLAG;
+  int blink_flag_length = strlen(blink_flag);
+  int step = (millis()/100) % (blink_flag_length * 8);
+  pinMode(BLINKLED, ((blink_flag[((int)step/8)] >> (step%8))) & 1);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  tick_blink();
 }
